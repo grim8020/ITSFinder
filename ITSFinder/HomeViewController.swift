574cc4084928e320647d6a2e1dfcf7be25940208
Shane Grim <grim8020@gmail.com>
2022-04-16 13:33:09 -0400

    Update home page

---
+++ ITSFinder/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ITSFinder
+//
+//  Created by Shane Grim on 4/14/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
+
+
+
+
+
